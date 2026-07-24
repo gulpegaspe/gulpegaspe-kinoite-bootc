55aad990e5539294f698018dc3e2727402b66739
gulpegaspe <62743226+gulpegaspe@users.noreply.github.com>
2026-07-24 18:38:12 +0200

    Delete systemd/scripts directory

--- systemd/scripts/first-boot-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Esempio: aggiunge una riga a /etc/fstab se non esiste già
-FSTAB_ENTRY="/dev/sdb1 /mnt/dati ext4 defaults 0 2"
-
-if ! grep -q "/dev/sdb1" /etc/fstab; then
-    echo "$FSTAB_ENTRY" >> /etc/fstab
-    echo "Fstab aggiornato con successo."
-fi
